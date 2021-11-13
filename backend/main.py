1b7f14561957ad8261627f78ed155051266fb671
Christopher <ceaphx19401@yahoo.com>
2021-11-12 20:51:01 -0700

    First Hello Commit

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_TITLE,
+    version=settings.PROJECT_VERSION
+    )
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
